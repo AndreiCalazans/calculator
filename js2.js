bbb94e11fdf69eca7311f4b1daff91ca4dd955db
AndreiCalazans <andreixoc@hotmail.com>
2016-12-03 06:55:47 -0200

    finished with the mechanics

--- js2.js
+++ js2.js
@@ -18,3 +18,27 @@ function displayvalue(n){
   }
   calcform.display.value = calcform.display.value.concat(n);
 }
+function clearEntry(){
+  calcform.display.value = calcform.display.value.substr(0 , calcform.display.value.length - 1 );
+}
+function allClear(){
+  calcform.display.value = "";
+  answer = "";
+}
+
+document.addEventListener("keydown", function(n){
+  console.log(n);
+  if(/[0-9/*+-]/.test(n.key)){
+    if (/[/*+-]/.test(n.key) && typeof(answer) == "number" ){
+      calcform.display.value = answer;
+      answer = "";
+    }
+
+    calcform.display.value = calcform.display.value.concat(n.key);
+
+  }else if (n.keyCode == 13){
+    result();
+  }else if(n.which == 8){
+    clearEntry();
+  }
+})
